d2726852f7627bc617a03b6a47c6adb14dba6f6f
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-05-16 20:41:54 +0700

    Form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -45,14 +45,40 @@ HandleOnMoverOver(event){
 
 }
 
+// dung de khi nhap gia tri vao button thong tin se thay doi theo nhung gi nhap trong button
+handleOnChangeInput = (event) =>{
+// alert('me') //khi nhap bat ki ki tu nao thi no se hien thong bao ra la 'me'
+this.setState({
+    name: event.target.value
+})
+// console.log(event.target.value) // .target.value boi vi value la web API tro ve gia tri cho minh luon
+
+}
+
+
+handleOnSumbit =(event) =>{
+    event.preventDefault();//ngan chan reload lai trang
+    console.log(this.state)
+    
+    // alert('me') //khi minh nhan enter no se hien ra thong bao la da luu thong tin 
+}
+
     render(){
         return(
-            <div> my first component
+            <div> 
                 {/* {Math.random()} */}
             
-                My name is {this.state.name} and I'm from {this.state.address} and I'm {this.state.age}
-                <button onMouseMove={this.HandleOnMoverOver}>Hover Me</button>
-                <button onClick={(event) => {this.handleClick(event)}}>Click Me</button>
+                My name is {this.state.name} / and I'm from {this.state.address} /and I'm {this.state.age}
+                {/* <button onMouseMove={this.HandleOnMoverOver}>Hover Me</button>  khi di chuyen no se hien con tro dang di chuyen den button do*/}
+                {/* <button onClick={(event) => {this.handleClick(event)}}>Click Me</button>  cai ket hop dung khi ban click vao no se thay doi thong tin theo yeu cau ben tren*/}
+                <form onSubmit={(event) => this.handleOnSumbit(event)}>
+                    <input type="text"
+                    onChange={(event) => this.handleOnChangeInput(event)}
+
+                                
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
 
         );
